334201a0763d4df98deb335cdace8fe04466daca
Jon Smith <aardvarkjon94@yahoo.com>
2021-03-31 13:12:53 -0500

    Added form. Modified post route to handle data

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path')
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
 
 app.get('/tacos', (req, res) => {
-    res.send("GET /TACOS RESPONSE")
+    res.render('tacos');
 })
 
 app.post('/tacos', (req, res) => {
-    res.send("POST /TACOS RESPONSE")
+    const { meat, qty } = req.body;
+    res.send(`OK, here are your ${qty} ${meat} tacos`)
 })
 
 app.listen(port, () => {
